ce97a91484afd3568b40eaebd14ce18c4d571baf
0hscar <oscar.weber00@gmail.com>
2022-11-13 17:05:39 +0200

    cls function + uppdates

--- DBGames.py
+++ DBGames.py
@@ -1,10 +1,14 @@
 
 from re import I
 import random
+from threading import Timer
 from unittest import expectedFailure
+import os
+
 
 '''RPS Variables'''
 rpsChoices = [1,2,3]
+rpsChoicesNames = ["Rock", "Scissors", "Paper"]
 playerScore = int(0)
 computerScore = int(0)
 
@@ -12,29 +16,38 @@ computerScore = int(0)
 
 '''Optimus Prime Variables'''
 
+'''Game Functions'''
+def cls():
+    os.system("cls" if os.name =="nt" else 'clear')
+
+
+    
+
+dcls = Timer(3, cls)
+
 '''RSP Functions'''
-def rspCheckResult(player, computer):
+def rspCheckResult(player1, player2, player, computer):
     roundWinner = 0
     if player in (rpsChoices):
         if player == computer:
             print("\nDraw!")
         elif player == 1 and computer == 2:
-            print("\npPlayer 1 chose rock, player 2 chose scissors, player 1 won!")
+            # print("\n", player1,"chose rock,", player2,"chose scissors,", player1,"won!")
             roundWinner = 1
         elif player == 1 and computer == 3:
-            print("\nPlayer 1 chose rock, player 2 chose paper, player 2 won!")
+            # print("\n", player1,"chose rock,", player2,"chose paper,", player2,"won!")
             roundWinner = 2
         elif player == 2 and computer == 1: 
-            print("\nPlayer 1 chose scissors, player 2 chose rock, player 2 won!")
+            # print("\n", player1,"chose scissors,", player2,"chose rock,", player2,"won!")
             roundWinner = 2
         elif player == 2 and computer == 3:
-            print("\nPlayer 1 chose scissors, player 2 chose paper, player 1 won!")
+            # print("\n", player1,"chose scissors,", player2,"chose paper,", player1,"won!")
             roundWinner = 1
         elif player == 3 and computer == 1:
-            print("\nPlayer 1 chose paper, player 2 chose rock, player 1 won!")
+            # print("\n", player1,"chose paper,", player2,"chose rock,", player1,"won!")
             roundWinner = 1
         elif player == 3 and computer == 2:
-            print("\nPlayer 1 chose paper, player 2 chose scissors, player 2 won!")
+            # print("\n", player1,"chose paper,", player2,"chose scissors,", player2,"won!")
             roundWinner = 2
     else:
         print("\nPlease choose between (1:Rock) (2:Scissors) (3:Paper)\n")
@@ -46,19 +59,21 @@ def rspCheckResult(player, computer):
 
 '''Main Code'''
 while True:
+    cls()
     print("\n**********\nMain Menu\n**********")
     MenuChoice = input("What do you want to play? :\nRPS, Mind Reader or Optimus Prime (no caps)\n")
     
     if MenuChoice == "exit":
+        print("Leaving...")
         break
     
     elif MenuChoice == "rps":
         '''RPS Game Code'''
-        
+        cls()
         while True:
-            print("\nWelcome to RPS!\n")
-            player1 = input("Player 1 name: \n")
-            player2 = input("Player 2 name: (enter a zero if you want to play against computer) \n")
+            print("\n***************\nWelcome to RPS!\n***************")
+            player1 = input("Player 1 name: ")
+            player2 = input("Player 2 name: (enter a zero if you want to play against computer) ")
             if player2 == "0":
                 player2 = "Computer"
             while True:
@@ -67,28 +82,35 @@ while True:
                     break
                 except:
                     print("Not a number")
-            
+            cls()
             while playerScore<rounds and computerScore<rounds:
-                while True:
-                    try:
-                        playerChoice = int(input("Player 1 choose: \n1:Rock\n2:Scissors\n3:Paper\n"))
-                        break
-                    except:
-                        print("Not a number")
-                computerChoice = random.randint(1,3) if player2 == "Computer" else int(input("Player 2 choose: \n1:Rock\n2:Scissors\n3:Paper\n"))
+              
                 
-                roundWinner = rspCheckResult(playerChoice, computerChoice)
-                
-                if roundWinner == 1:
-                    playerScore += 1
-                else:
-                    computerScore += 1
-                    
                 print("\n",player1, ":", playerScore, "\n",player2,":",computerScore,"\n")
+                        
+                playerChoice = input(F"{player1} choose: \n1:Rock\n2:Scissors\n3:Paper\n")
+                computerChoice = input(F"{player2} choose: \n1:Rock\n2:Scissors\n3:Paper\n") if player2 != "Computer" else str(random.randint(1,3)) 
+                #Changes the random to string so I can check them both in the if statement, otherwise I would have seperate checks for them
+                cls()
+                if playerChoice == "exit" or computerChoice == "exit":
+                    print("Leaving...")
+                    break
+                elif playerChoice.isdigit() and computerChoice.isdigit():
+                    roundWinner = rspCheckResult(player1, player2, int(playerChoice), int(computerChoice))
+                    
+                    if roundWinner == 1:
+                        print(player1, "chose", rpsChoicesNames[int(playerChoice)-1], ",", player2, "chose", rpsChoicesNames[int(computerChoice)-1], ":", player1, "won the round!")
+                        playerScore += 1
+                    elif roundWinner == 2:
+                        print(player1, "chose", rpsChoicesNames[int(playerChoice)-1], ",", player2, "chose", rpsChoicesNames[int(computerChoice)-1], ":", player2, "won the round!")                      
+                        computerScore += 1
+                    
+                else:
+                    print("Invalid input")
                 
-            print("Winner: ", player1 if playerScore > computerScore else player2)
-            
-            if input("Do you want to play again? (1: Yes, Anything else: No): ") == "1":
+                
+            print("\nWinner: ", player1 if playerScore > computerScore else player2)
+            if input("\nDo you want to play again? (1: Yes, Anything else: No): ") == "1":
                 computerScore = 0
                 playerScore = 0
                 continue
@@ -96,6 +118,7 @@ while True:
         
     elif MenuChoice == "mind reader":
         '''Mind Reader Code'''
+        cls()
         while True:
             print("\nWelcome to Mind Reader!\n")
             while True:
@@ -107,50 +130,27 @@ while True:
             print("\nGo ahead, try to guess the number I am thinking about")
             randNumber = random.randint(1,diff*10)
             guesses = 0
-
+            cls()   
             while True:
+                             
                 userGuess = input("Guess: ")
                 if userGuess == "exit":
                     print("Leaving...")
                     break
                 elif userGuess.isdigit():
                     if int(userGuess) == randNumber:
-                        print("You chose wisely, well done!\nIt took you", guesses+1,"guess(es) to get it right.")
+                        print("\nYou chose wisely, well done!\nIt took you", guesses+1,"guess(es) to get it right.")
                         break
                     print("You need to go", 'higher' if int(userGuess) < randNumber else 'lower')
                     guesses += 1
-                    print("Guesses:", guesses)
+                    print("Guesses:", guesses, "\n")
+                    
                 else:
                     print("Incorrect input")
                 
             if input("\nDo you want to play again (1: Yes, Anything else: No): ") == "1":
                 continue
             else: break 
-                
-            
-            # elif userGuess < randNumber:
-            #     print("Too low fella!")
-            # elif userGuess >
-            
-            # F'{oneTerm} + {twoTerm} ='
-            
-# >>> raining = False
-# >>> print("Let's go to the", 'beach' if not raining else 'library')
-# Let's go to the beach
-# >>> raining = True
-# >>> print("Let's go to the", 'beach' if not raining else 'library')
-# Let's go to the library
-
-# >>> age = 12
-# >>> s = 'minor' if age < 21 else 'adult'
-# >>> s
-# 'minor'
-
-# >>> 'yes' if ('qux' in ['foo', 'bar', 'baz']) else 'no'
-# 'no'
-            
-            
-            
         
 
     elif MenuChoice == "optimus prime":
